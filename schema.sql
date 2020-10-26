7ba6e141a2cf15c440b2343025d98ebc43ec4084
Vincent Diaz <Vincent-Diaz@users.noreply.github.com>
2020-10-26 00:06:22 -0500

    added auto increment to primary id

--- schema.sql
+++ schema.sql
@@ -1,24 +1,40 @@
-DROP DATABASE IF EXISTS empolyee_trackerDB;
-CREATE database empolyee_trackerDB;
+DROP DATABASE IF EXISTS employees_trackerDB;
+CREATE database employees_trackerDB;
 
-USE empolyee_trackerDB;
+USE employees_trackerDB;
 
-CREATE TABLE department = (
-    id INT PRIMARY KEY NOT NULL,
-    name VARCHAR(50) NOT NULL
+CREATE TABLE department (
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    names VARCHAR(50) NOT NULL
 );
 
-CREATE TABLE role = (
-    id INT PRIMARY KEY,
+CREATE TABLE roles (
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
     title VARCHAR(50),
-    salary DECIMAL(7, 3) NOT NULL,
-    department_id INT NOT NULL FOREIGN KEY
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    FOREIGN KEY(department_id) REFERENCES department(id)
 );
 
-CREATE TABLE empployee = (
-    id INT NOT NULL PRIMARY KEY,
+CREATE TABLE employee (
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
-    role_id INT NOT NULL FOREIGN KEY,
-    manager_id INT NULL FOREIGN KEY
+    role_id INT NOT NULL,
+    manager_id INT NULL,
+    FOREIGN KEY (role_id) REFERENCES roles(id),
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
+
+SELECT * FROM department;
+SELECT * FROM roles;
+SELECT * FROM employee
+-- department seeds --
+-- INSERT INTO department (names)
+-- VALUES ("Sales");
+-- INSERT INTO department (names)
+-- VALUES ("Human Resources");
+-- INSERT INTO department (names)
+-- VALUES ("Finance");
+-- INSERT INTO department (names)
+-- VALUES ("Quality Assurance");
