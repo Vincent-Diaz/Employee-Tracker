cecf0301268eae7374ce057de05d04c0848c5de3
Vincent Diaz <Vincent-Diaz@users.noreply.github.com>
2020-10-26 01:17:41 -0500

    added view emp, dep, roles functions added default case

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
+const cTable = require("console.table");
 
 const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
     password: "Diaz5205",
-    database: "employee_trackerDB"
+    database: "employees_trackerDB"
 });
 
 connection.connect(function (err) {
@@ -28,8 +29,8 @@ function start() {
             "add department",
             "add role",
             "remove employee",
-            "update employee role"
-        ]
+            "update employee role",
+            "Exit"]
     }).then(answers => {
         console.log(answers.choice);
         switch (answers.choice) {
@@ -65,7 +66,40 @@ function start() {
                 updateEmployee();
                 break;
 
+            case "Exit":
+                endApp();
+                break;
+
+            default:
+                break;
+
         }
     })
 }
 
+function viewEmployees() {
+    connection.query("SELECT emp.first_name, emp.last_name, roles.title, department.names AS departments, roles.salary, CONCAT(mngr.first_name, ' ', mngr.last_name) AS manager FROM employee AS emp INNER JOIN roles ON emp.role_id = roles.id INNER JOIN department ON roles.department_id = department.id LEFT JOIN employee AS mngr ON emp.manager_id = mngr.id;",
+        function (err, res) {
+            if (err) throw err;
+            console.table(res)
+            start();
+        })
+}
+
+function viewDepartments() {
+    connection.query("SELECT * FROM department;",
+        function (err, res) {
+            if (err) throw err;;
+            console.table(res)
+            start();
+        })
+}
+
+function viewRoles() {
+    connection.query("SELECT * FROM roles;",
+        function (err, res) {
+            if (err) throw err;;
+            console.table(res)
+            start();
+        })
+}
